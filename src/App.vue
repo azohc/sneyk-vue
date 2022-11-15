9a14282b15115da6d469bf9c9a7f1d86c54d3698
juan c <juanchozass@gmail.com>
2022-11-15 12:39:21 +0100

    init snake

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import Title from "./components/Title.vue";
+
+const canvas = ref(null);
+onMounted(() => {
+  console.log("mounted", canvas.value);
+  const context = canvas.value.getContext("2d");
+  console.log("mounted", context);
+  const canvasBounds = canvas.value.getBoundingClientRect();
+  // Box width
+  const bw = canvasBounds.width;
+  // Box height
+  const bh = canvasBounds.height;
+  // Padding
+  const pad = 0;
+  // Gap
+  const cellSize = 4;
+  // Draw first line
+  const drawFirst = false;
+  for (let x = drawFirst ? 0 : 1; x <= bw; x += cellSize) {
+    context.moveTo(0.5 + x + pad, pad);
+    context.lineTo(0.5 + x + pad, bh + pad);
+  }
+  for (let x = drawFirst ? 0 : 1; x <= bh; x += cellSize) {
+    context.moveTo(pad, 0.5 + x + pad);
+    context.lineTo(bw + pad, 0.5 + x + pad);
+  }
+  context.strokeStyle = "black";
+  context.stroke();
+});
+</script>
+
+<template>
+  <div class="text-center selection:bg-green-100">
+    <Title> sneyk </Title>
+  </div>
+  <canvas ref="canvas" class="h-3/5 border border-green-900" />
+</template>
+
+<style></style>
---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  msg: String,
+});
+</script>
+
+<template>
+  <h1
+    class="bg-gradient-to-r from-green-100 to-green-600 bg-clip-text text-8xl font-black text-transparent selection:bg-transparent"
+  >
+    <slot></slot>
+  </h1>
+</template>
+
+<style scoped></style>
