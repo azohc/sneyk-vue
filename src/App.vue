71a7f248d57815c103a0142cbeea34e6f45149b6
juan c <juanchozass@gmail.com>
2022-12-01 15:25:50 +0100

    Canvas is green

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,67 @@
 <script setup>
 import { computed, onMounted, ref } from "vue";
 
-const width = 1000;
-const height = 500;
-const widthpx = computed(() => `${width}px`);
-const heightpx = computed(() => `${height}px`);
+const CANVAS_WIDTH = 1000;
+const CANVAS_HEIGHT = 500;
+const CANVAS_WIDTH_PX = computed(() => `${CANVAS_WIDTH}px`);
+const CANVAS_HEIGHT_PX = computed(() => `${CANVAS_HEIGHT}px`);
+const CELL_SIZE = 5;
 
 const canvas = ref(null);
 
 onMounted(() => {
-  console.log("mounted", canvas.value);
-  const context = canvas.value.getContext("2d");
-  console.log("mounted", context);
-  const canvasBounds = canvas.value.getBoundingClientRect();
-  // Box width
-  const bw = canvasBounds.width;
-  // Box height
-  const bh = canvasBounds.height;
-  // Gap
-  const cellSize = 5;
-  // Draw first line
-  const drawFirst = true;
-  for (let x = drawFirst ? 0 : 1; x <= bw; x += cellSize) {
-    context.moveTo(x, 0);
-    context.lineTo(x, bh);
-  }
-  for (let x = drawFirst ? 0 : 1; x <= bh; x += cellSize) {
-    context.moveTo(0, x);
-    context.lineTo(bw, x);
-  }
-  context.strokeStyle = "black";
-  context.stroke();
+  const gameLayerCtx = canvas.value.getContext("2d", {
+    alpha: false,
+  });
+
+  fillBg(gameLayerCtx, "#4F822B");
+
+  drawGrid(gameLayerCtx, {
+    drawFirstLine: true,
+    color: "#00000088",
+  });
 });
+
+const fillBg = (context, color) => {
+  context.fillStyle = color;
+  context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+};
+
+const drawGrid = (context, options) => {
+  const { color, drawFirstLine } = options;
+  for (
+    let x = drawFirstLine ? 0 : 1;
+    x <= CANVAS_WIDTH;
+    x += CELL_SIZE
+  ) {
+    context.moveTo(x, 0);
+    context.lineTo(x, CANVAS_HEIGHT);
+  }
+  for (
+    let x = drawFirstLine ? 0 : 1;
+    x <= CANVAS_HEIGHT;
+    x += CELL_SIZE
+  ) {
+    context.moveTo(0, x);
+    context.lineTo(CANVAS_WIDTH, x);
+  }
+  context.strokeStyle = color;
+  context.stroke();
+};
 </script>
 
 <template>
   <canvas
     ref="canvas"
-    :height="height"
-    :width="width"
-    class="mi-auto border border-black"
+    :height="CANVAS_HEIGHT"
+    :width="CANVAS_WIDTH"
+    class="mi-auto border border-white"
   />
 </template>
 
 <style>
 canvas {
-  height: v-bind(heightpx);
-  width: v-bind(widthpx);
+  height: v-bind(CANVAS_HEIGHT_PX);
+  width: v-bind(CANVAS_WIDTH_PX);
 }
 </style>
