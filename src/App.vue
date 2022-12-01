fdb2d86207f9065b90a1644ac80c8e3bbea19702
juan c <juanchozass@gmail.com>
2022-12-01 16:21:42 +0100

    Likkle snake

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,59 @@ onMounted(() => {
 
   fillBg(gameLayerCtx, "#4F822B");
 
+  drawSnake(gameLayerCtx, 11, 11, "#213404");
+
   drawGrid(gameLayerCtx, {
     drawFirstLine: true,
-    color: "#00000088",
+    color: "#4F822B88",
   });
 });
+
+const drawSnake = (context, x, y, color) => {
+  // TODO draw snake: 1 'pixelset' for tail, 1 for body, one for head. 1 for tongue?
+
+  //HEAD
+  // MOUTH
+  drawPixel(context, x + 1, y + 1, color);
+  drawPixel(context, x + 1, y + 2, color);
+  drawPixel(context, x + 2, y + 1, color);
+  drawPixel(context, x + 2, y + 2, color);
+  // EYE
+  drawPixel(context, x + 3, y + 2, color);
+  drawPixel(context, x + 3, y + 0, color);
+  drawPixel(context, x + 4, y + 1, color);
+  drawPixel(context, x + 4, y + 2, color);
+
+  //BODY
+  // ONE SEGMENT (STRIPED)
+  drawPixel(context, x + 5, y + 2, color);
+  drawPixel(context, x + 6, y + 1, color);
+  // ONE SEGMENT (SOLID)
+  drawPixel(context, x + 7, y + 1, color);
+  drawPixel(context, x + 7, y + 2, color);
+  drawPixel(context, x + 8, y + 1, color);
+  drawPixel(context, x + 8, y + 2, color);
+
+  // TAIL
+  drawPixel(context, x + 9, y + 2, color);
+  drawPixel(context, x + 10, y + 1, color);
+  drawPixel(context, x + 11, y + 1, color);
+  drawPixel(context, x + 11, y + 2, color);
+  drawPixel(context, x + 12, y + 1, color);
+  drawPixel(context, x + 12, y + 2, color);
+  drawPixel(context, x + 13, y + 2, color);
+  drawPixel(context, x + 14, y + 2, color);
+};
+
+const drawPixel = (context, x, y, color) => {
+  context.fillStyle = color;
+  context.fillRect(
+    x * CELL_SIZE,
+    y * CELL_SIZE,
+    CELL_SIZE,
+    CELL_SIZE
+  );
+};
 
 const fillBg = (context, color) => {
   context.fillStyle = color;
