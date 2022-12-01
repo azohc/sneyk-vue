1c5b765369396f4f18a92d5360c52b60f08f562e
juan c <juanchozass@gmail.com>
2022-12-01 14:13:10 +0100

    Square one: grid divided evenly

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
-import { onMounted, ref } from "vue";
-import Title from "./components/Title.vue";
+import { computed, onMounted, ref } from "vue";
+
+const width = 1000;
+const height = 500;
+const widthpx = computed(() => `${width}px`);
+const heightpx = computed(() => `${height}px`);
 
 const canvas = ref(null);
+
 onMounted(() => {
   console.log("mounted", canvas.value);
   const context = canvas.value.getContext("2d");
@@ -12,19 +17,17 @@ onMounted(() => {
   const bw = canvasBounds.width;
   // Box height
   const bh = canvasBounds.height;
-  // Padding
-  const pad = 0;
   // Gap
-  const cellSize = 4;
+  const cellSize = 5;
   // Draw first line
-  const drawFirst = false;
+  const drawFirst = true;
   for (let x = drawFirst ? 0 : 1; x <= bw; x += cellSize) {
-    context.moveTo(0.5 + x + pad, pad);
-    context.lineTo(0.5 + x + pad, bh + pad);
+    context.moveTo(x, 0);
+    context.lineTo(x, bh);
   }
   for (let x = drawFirst ? 0 : 1; x <= bh; x += cellSize) {
-    context.moveTo(pad, 0.5 + x + pad);
-    context.lineTo(bw + pad, 0.5 + x + pad);
+    context.moveTo(0, x);
+    context.lineTo(bw, x);
   }
   context.strokeStyle = "black";
   context.stroke();
@@ -32,10 +35,17 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="text-center selection:bg-green-100">
-    <Title> sneyk </Title>
-  </div>
-  <canvas ref="canvas" class="h-3/5 border border-green-900" />
+  <canvas
+    ref="canvas"
+    :height="height"
+    :width="width"
+    class="mi-auto border border-black"
+  />
 </template>
 
-<style></style>
+<style>
+canvas {
+  height: v-bind(heightpx);
+  width: v-bind(widthpx);
+}
+</style>
