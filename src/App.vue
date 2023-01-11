8d4c0eb6267b58c205674ee98b80c4e5ec919434
juancs <juanchozass@gmail.com>
2023-01-11 19:58:19 +0100

    Movement in all directions is mirroring OK

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ const DIR_DOWN = "DOWN";
 const DIR_LEFT = "LEFT";
 const DIR_RIGHT = "RIGHT";
 
-const TICKRATE = 1000;
+const TICKRATE = 150;
 
 const CANVAS_WIDTH = 1000;
 const CANVAS_HEIGHT = 500;
@@ -23,25 +23,10 @@ const canvas = ref(null);
 
 // prettier-ignore
 const spawnPos = {
-  x: 100,
-  y: 46
-};
-const spawnPosCenter = {
   x: CANVAS_WIDTH / PIXEL_SIZE / SQUARE_SIZE,
   y: CANVAS_HEIGHT / PIXEL_SIZE / SQUARE_SIZE,
 };
-
-const initleftdirstate = {
-  coords: [
-    spawnPos,
-    { ...spawnPos, x: spawnPos.x + SQUARE_SIZE },
-    { ...spawnPos, x: spawnPos.x + 2 * SQUARE_SIZE },
-    { ...spawnPos, x: spawnPos.x + 3 * SQUARE_SIZE },
-  ],
-  dir: DIR_LEFT,
-};
-
-const initrightdirstate = {
+const snakeState = ref({
   coords: [
     spawnPos,
     { ...spawnPos, x: spawnPos.x - SQUARE_SIZE },
@@ -49,30 +34,7 @@ const initrightdirstate = {
     { ...spawnPos, x: spawnPos.x - 3 * SQUARE_SIZE },
   ],
   dir: DIR_RIGHT,
-};
-
-const initupdirstate = {
-  coords: [
-    spawnPos,
-    { ...spawnPos, y: spawnPos.y + SQUARE_SIZE },
-    { ...spawnPos, y: spawnPos.y + 2 * SQUARE_SIZE },
-    { ...spawnPos, y: spawnPos.y + 3 * SQUARE_SIZE },
-  ],
-  dir: DIR_UP,
-};
-
-const initdowndirstate = {
-  coords: [
-    spawnPos,
-    { ...spawnPos, y: spawnPos.y - SQUARE_SIZE },
-    { ...spawnPos, y: spawnPos.y - 2 * SQUARE_SIZE },
-    { ...spawnPos, y: spawnPos.y - 3 * SQUARE_SIZE },
-  ],
-  dir: DIR_DOWN,
-};
-
-const snakeState = ref(initdowndirstate);
-// const snakeState = initdowndirstate;
+});
 const latestKeydown = ref(null);
 
 onMounted(() => {
@@ -85,10 +47,11 @@ onMounted(() => {
   drawSnake();
 
   playing = setInterval(onTick, TICKRATE);
-  // drawGrid({
-  //   drawFirstLine: true,
-  //   color: "#4F822B88",
-  // });
+
+  drawGrid({
+    drawFirstLine: true,
+    color: "#4F822B88",
+  });
 });
 
 const onTick = () => {
@@ -108,27 +71,11 @@ const onTick = () => {
   drawSnake();
 };
 
-const drawGreenSquare = (x, y, striped) => {
-  if (striped) {
-    if (snakeState.value.dir === DIR_LEFT) {
-      /// ONE SQUARE SLANT = \
-      // bot left
-      drawGreenPx(x, y + 2);
-      // top right
-      drawGreenPx(x + 1, y + 1);
-    } else if (snakeState.value.dir === DIR_RIGHT) {
-      /// ONE SQUARE SLANT = /
-      // bot right
-      drawGreenPx(x + 1, y + 2);
-      // top left
-      drawGreenPx(x, y + 1);
-    }
-  } else {
-    drawGreenPx(x, y + 1);
-    drawGreenPx(x, y + 2);
-    drawGreenPx(x + 1, y + 1);
-    drawGreenPx(x + 1, y + 2);
-  }
+const drawSquare = (x, y, color) => {
+  drawPx(x, y, color);
+  drawPx(x, y + 1, color);
+  drawPx(x + 1, y, color);
+  drawPx(x + 1, y + 1, color);
 };
 
 const drawSnake = () => {
@@ -145,116 +92,68 @@ const drawSnake = () => {
     return yc;
   };
 
-  const { coords, dir } = snakeState.value;
+  const { coords } = snakeState.value;
   const len = coords.length;
   const head = coords[0];
-  const tail = coords[len - 1];
 
-  /// HEAD
-  // MOUTH
-  drawGreenSquare(head.x, head.y);
-  // EYE
-  drawEye(head);
-  //// BODY
-  const stripeFrequency = 2;
-  coords.slice(1, len - 1).forEach((coord, i) => {
-    if (DIR_UP === dir) {
-      drawGreenSquare(wrapxc(coord.x), wrapyc(coord.y), !(i % stripeFrequency));
-    } else if (DIR_DOWN === dir) {
-      drawGreenSquare(wrapxc(coord.x), wrapyc(coord.y), !(i % stripeFrequency));
-    } else if (DIR_LEFT === dir) {
-      drawGreenSquare(wrapxc(coord.x + SQUARE_SIZE), wrapyc(coord.y), !(i % stripeFrequency));
-    } else if (DIR_RIGHT === dir) {
-      drawGreenSquare(wrapxc(coord.x - SQUARE_SIZE), wrapyc(coord.y), !(i % stripeFrequency));
-    }
-  });
-
-  /// TAIL
-  // ONE SEGMENT (STRIPED)
-  if (dir === DIR_UP) {
-    drawGreenPx(wrapxc(tail.x), wrapyc(tail.y));
-  } else if (dir === DIR_LEFT) {
-    drawGreenPx(wrapxc(tail.x + 2), wrapyc(tail.y + 2));
-    drawGreenPx(wrapxc(tail.x + 3), wrapyc(tail.y + 1));
-    drawGreenPx(wrapxc(tail.x + 4), wrapyc(tail.y + 2));
-    drawGreenPx(wrapxc(tail.x + 5), wrapyc(tail.y + 2));
-  } else if (dir === DIR_RIGHT) {
-    drawGreenPx(wrapxc(tail.x - 2 * SQUARE_SIZE + 3), wrapyc(tail.y + 2));
-    drawGreenPx(wrapxc(tail.x - 2 * SQUARE_SIZE + 2), wrapyc(tail.y + 1));
-    drawGreenPx(wrapxc(tail.x - 2 * SQUARE_SIZE + 1), wrapyc(tail.y + 2));
-    drawGreenPx(wrapxc(tail.x - 2 * SQUARE_SIZE), wrapyc(tail.y + 2));
-  }
-
-  function drawEye(head) {
-    switch (dir) {
-      case DIR_UP:
-        drawGreenPx(head.x + 1, wrapyc(head.y + SQUARE_SIZE + 1));
-        drawGreenPx(head.x - 1, wrapyc(head.y + SQUARE_SIZE + 1));
-        drawGreenPx(head.x, wrapyc(head.y + SQUARE_SIZE + 2));
-        drawGreenPx(head.x + 1, wrapyc(head.y + SQUARE_SIZE + 2));
-        break;
-      case DIR_DOWN:
-        drawGreenPx(head.x, wrapyc(head.y));
-        drawGreenPx(head.x + 2, wrapyc(head.y));
-        drawGreenPx(head.x, wrapyc(head.y - 1));
-        drawGreenPx(head.x + 1, wrapyc(head.y - 1));
-        break;
-      case DIR_LEFT:
-        drawGreenPx(wrapxc(head.x + SQUARE_SIZE), head.y);
-        drawGreenPx(wrapxc(head.x + SQUARE_SIZE), head.y + 2);
-        drawGreenPx(wrapxc(head.x + SQUARE_SIZE + 1), head.y + 1);
-        drawGreenPx(wrapxc(head.x + SQUARE_SIZE + 1), head.y + 2);
-        break;
-      case DIR_RIGHT:
-        drawGreenPx(wrapxc(head.x - SQUARE_SIZE), head.y + 1);
-        drawGreenPx(wrapxc(head.x - SQUARE_SIZE), head.y + 2);
-        drawGreenPx(wrapxc(head.x - SQUARE_SIZE + 1), head.y);
-        drawGreenPx(wrapxc(head.x - SQUARE_SIZE + 1), head.y + 2);
-        break;
-      default:
-        console.debug("default");
-    }
-  }
+  drawSquare(head.x, head.y);
+  coords
+    .slice(1, len - 1)
+    .forEach((coord) =>
+      drawSquare(wrapxc(coord.x), wrapyc(coord.y), "red")
+    );
 };
 
 function unshiftNewHeadCoord() {
-  const currentHead = snakeState.value.coords[0];
+  const head = snakeState.value.coords[0];
   switch (snakeState.value.dir) {
     case DIR_UP:
       snakeState.value.coords.unshift({
-        x: currentHead.x,
-        y: currentHead.y > 0 ? currentHead.y - SQUARE_SIZE : CANVAS_HEIGHT / PIXEL_SIZE - SQUARE_SIZE,
+        x: head.x,
+        y:
+          head.y > 0
+            ? head.y - SQUARE_SIZE
+            : CANVAS_HEIGHT / PIXEL_SIZE - SQUARE_SIZE,
       });
       break;
     case DIR_DOWN:
       snakeState.value.coords.unshift({
-        x: currentHead.x,
-        y: currentHead.y > 0 ? currentHead.y + SQUARE_SIZE : CANVAS_HEIGHT / PIXEL_SIZE + SQUARE_SIZE,
+        x: head.x,
+        y:
+          head.y + SQUARE_SIZE < CANVAS_HEIGHT / PIXEL_SIZE
+            ? head.y + SQUARE_SIZE
+            : 0,
       });
       break;
     case DIR_LEFT:
       snakeState.value.coords.unshift({
-        x: currentHead.x > 0 ? currentHead.x - SQUARE_SIZE : CANVAS_WIDTH / PIXEL_SIZE - SQUARE_SIZE,
-        y: currentHead.y,
+        x:
+          head.x > 0
+            ? head.x - SQUARE_SIZE
+            : CANVAS_WIDTH / PIXEL_SIZE - SQUARE_SIZE,
+        y: head.y,
       });
       break;
     case DIR_RIGHT:
       snakeState.value.coords.unshift({
-        x: currentHead.x + SQUARE_SIZE < CANVAS_WIDTH / PIXEL_SIZE ? currentHead.x + SQUARE_SIZE : 0,
-        y: currentHead.y,
+        x:
+          head.x + SQUARE_SIZE < CANVAS_WIDTH / PIXEL_SIZE
+            ? head.x + SQUARE_SIZE
+            : 0,
+        y: head.y,
       });
       break;
   }
 }
 
-const drawGreenPx = (x, y) => {
-  context.fillStyle = "#213404";
-  context.fillRect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
-};
-
-const drawRedPx = (x, y) => {
-  context.fillStyle = "red";
-  context.fillRect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+const drawPx = (x, y, color) => {
+  context.fillStyle = color ? color : "#213404";
+  context.fillRect(
+    x * PIXEL_SIZE,
+    y * PIXEL_SIZE,
+    PIXEL_SIZE,
+    PIXEL_SIZE
+  );
 };
 
 const fillBg = (color) => {
@@ -264,11 +163,19 @@ const fillBg = (color) => {
 
 const drawGrid = (options) => {
   const { color, drawFirstLine } = options;
-  for (let x = drawFirstLine ? 0 : 1; x <= CANVAS_WIDTH; x += PIXEL_SIZE) {
+  for (
+    let x = drawFirstLine ? 0 : 1;
+    x <= CANVAS_WIDTH;
+    x += PIXEL_SIZE
+  ) {
     context.moveTo(x, 0);
     context.lineTo(x, CANVAS_HEIGHT);
   }
-  for (let x = drawFirstLine ? 0 : 1; x <= CANVAS_HEIGHT; x += PIXEL_SIZE) {
+  for (
+    let x = drawFirstLine ? 0 : 1;
+    x <= CANVAS_HEIGHT;
+    x += PIXEL_SIZE
+  ) {
     context.moveTo(0, x);
     context.lineTo(CANVAS_WIDTH, x);
   }
@@ -277,7 +184,6 @@ const drawGrid = (options) => {
 };
 
 const reset = () => {
-  snakeState.value.coords = initupdirstate.coords;
   context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   fillBg("#4F822B");
 };
@@ -290,7 +196,10 @@ const togglepause = () => {
   }
 };
 const onkeydown = (event) => {
-  console.log("invoke onkeydown");
+  if (event.code === "Space") {
+    togglepause();
+    return;
+  }
   const ACCEPTED_DIR_KEYCODES = [
     "ArrowUp",
     "ArrowDown",
@@ -332,10 +241,11 @@ const onkeydown = (event) => {
     ref="canvas"
     :height="CANVAS_HEIGHT"
     :width="CANVAS_WIDTH"
-    class="mi-auto border border-white"
+    class="mi-auto border-2"
   />
-  <button @click="reset">reset</button>
-  <button @click="togglepause">{{ playing ? "pause" : "resume" }}</button>
+  <button @click="togglepause">
+    {{ playing ? "pause" : "resume" }}
+  </button>
   <code class="text-center">
     {{ snakeState.dir }}
   </code>
